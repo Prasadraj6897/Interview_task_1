793548907b24732129fa26f64f59cb1df3404a08
Prasadraj6897 <prasadraj32895@gmail.com>
2021-06-12 16:47:16 +0530

    Created Chat APP

--- admin/src/components/OnlineUsers.jsx
+++ admin/src/components/OnlineUsers.jsx
@@ -14,12 +14,22 @@ const OnlineUsers = (props) => {
 				<div style={{ margin: "0 10px" }}>Online Users</div>
 			</div>
 			<ul className="users-list">
-				<li>
-				<span style={{ textTransform: "capitalize" }}>Prasad</span>
-					<span className="new-message-count">
-						2
-					</span>				
-				</li>	
+				{users &&
+					Object.keys(users).map((user, index) => (
+					<>
+						{user !== username ? (
+							<li key={user} onClick={() => onUserSelect(user)}>
+								<span style={{ textTransform: "capitalize" }}>{user}</span>
+								
+								{checkUnseenMessages(user) !== 0 ? (
+									<span className="new-message-count">
+										{checkUnseenMessages(user)}
+									</span>
+								) : null}
+							</li>
+						) : null}
+					</>
+				))}
 			</ul>
 		</div>
   );
--- server/index.js
+++ server/index.js
@@ -39,13 +39,36 @@ const io = require("socket.io")(httpServer, {
   },
 });
 
+const users = {};
+
 io.on("connection", (socket) => {
     console.log("someone connected and socket id " + socket.id);
 
     socket.on("disconnect", () => {
       console.log(`${socket.id} disconnected`);
+	  
+	  for (let user in users) {
+			if (users[user] === socket.id) {
+			delete users[user];
+			}
+		}
+		io.emit("all_users", users);
     });
 
+	socket.on("new_user", (username) => {
+		console.log("Server : " + username);
+		users[username] = socket.id;
+
+		//we can tell every other users someone connected
+		io.emit("all_users", users);
+	  });
+	  
+	  socket.on("send_message", (data) => {
+		console.log(data);
+	
+		const socketId = users[data.receiver];
+		io.to(socketId).emit("new_message", data);
+	  });
 });
 
 httpServer.listen(process.env.PORT, () => {
