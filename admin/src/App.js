a475478001a0d4ff1ba51f2768f9dd5247aa103a
Prasadraj6897 <prasadraj32895@gmail.com>
2021-06-12 10:52:48 +0530

    Added Logo and title and added font-family link in index.html

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,15 +1,23 @@
 import { io } from "socket.io-client";
+import logo from "./assets/chat.png";
+
+
+
+
 import './App.css';
 
 
 const socket = io(`http://localhost:8000`);
 
 function App() {
-  return (
-    <div className="App">
-      hello
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="app-header">
+				<img src={logo} alt="" />
+				<div className="app-name b-500 primaryColor">My Chat</div>
+			</header>
+		</div>
+	);
 }
 
 export default App;
